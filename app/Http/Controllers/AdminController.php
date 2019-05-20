a24ca51bdff901b1156a2f1b7cf28b0715fbccfb
Duha-alqausar <duha.alqausar@gmail.com>
2019-05-20 12:02:15 +0700

    Last4

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -122,7 +122,7 @@ class AdminController extends Controller
        ->Where('tanggal_absen', $date)->count();
 
        if ($q > 0) {
-       return redirect('/admin/permohonan')->with('status2', "$nama sudah absen,Permohonan Berhasil Di konfirmasi");
+       return redirect('/admin/permohonan')->with('status2', "Dia sudah absen,Permohonan berhasil Di konfirmasi");
     }else{
         $q = DB::table('absensi')->insert([
         'id_pegawai' => $absensi1,
