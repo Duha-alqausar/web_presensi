f64f93442bb0493f0e445347321268b4558e674c
Duha-alqausar <duha.alqausar@gmail.com>
2019-05-20 10:20:07 +0700

    fixeds2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,18 @@
-@extends('auth.app')
-@section('title','Sistem Presensi')
-@section('content')
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/login.css">
 
-<div class="content">
-    <div class="row">
+    <title>Sistem Absensi</title>
+  </head>
+  <body>
+<div class="content mt-5">
+    <div class="contentt">
         <div class="container">
           <div class="row justify-content-md-center" >
             <div class="col-md-6 col-sm-6">
@@ -13,8 +21,9 @@
                         @csrf
 
                         <div class="card shadow-lg">
-
                           <div class="card-body">
+                            <div class="card-title text-center"><h3><font> Selamat Datang</h3></font></div>
+                            <br>
                             <div class="form-group">
                                <label>E-Mail Address</label>
                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-mail">
@@ -23,14 +32,20 @@
                                <label>Password</label>
                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Password">
                            </div>
-                           <button type="submit" class="btn btn-black col-12">Login</button>
+                           <button type="submit" class="btn btn-dark col-12">LOGIN</button>
 
                        </form>
+                       <hr>
+                    <footer class="blockquote-footer">Silahkan Login <cite title="Source Title">untuk mengakses Sistem Absensi</cite></footer>
                    </div>
-
                </div>
            </div>
 
        </div>
-   </div>
-   @endsection
+   </div></div></div></div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
